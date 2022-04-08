db23ee2f9f841547b890c99dd53ff1c52e7e18f6
Sam Leffler <sleffler@google.com>
2022-04-08 17:41:26 +0000

    KataOS template support for CNode slot allocator.
    Add SELF_CNODE_FIRST_SLOT and SELF_CNODE_LAST_SLOT definitions for
    setting up a top-level CNode slot allocator.
    
    Change-Id: I73a541038f145ae37e7aedd364090e92374ba84a

--- camkes/templates/component.kataos.c
+++ camkes/templates/component.kataos.c
@@ -21,6 +21,10 @@
 #include <sel4/sel4.h>
 #include <sel4utils/mapping.h>
 
+/*- macro next_pow2(val) -*/
+    /*? pow(2, val.bit_length()) ?*/
+/*- endmacro -*/
+
 /*# TODO(sleffler): make more of these conditional? #*/
 /*- set self_cnode = alloc_cap('cnode', my_cnode, write=true) -*/
 const seL4_CPtr SELF_CNODE = /*? self_cnode ?*/;
@@ -50,3 +54,19 @@ const unsigned char RECV_CNODE_DEPTH = 5;
         /*- endif -*/
     /*- do my_cnode.__setattr__('headroom', my_cnode['headroom'] + headroom) -*/
 /*- endif -*/
+
+/*# No cap allocation from here on! We assume all caps exist so we can guess our cnode size from the
+ * holding slot #*/
+/*- set holding_slot = alloc_cap('temporary_kataos_slot', None) -*/
+const seL4_CPtr SELF_CNODE_FIRST_SLOT = /*? holding_slot ?*/;
+/*- if cap_space.cnode.size_bits == 'auto' -*/
+    /*- set size_bits = configuration[me.name].get('cnode_size_bits') -*/
+    /*- if size_bits is not none -*/
+        /*- set cnodesize = pow(2, size_bits) -*/
+    /*- else -*/
+        /*- set cnodesize = next_pow2(holding_slot + my_cnode['headroom']) -*/
+    /*- endif -*/
+/*- else -*/
+    /*- set cnodesize = pow2(cap_space.cnode.size_bits) -*/
+/*- endif -*/
+const seL4_CPtr SELF_CNODE_LAST_SLOT = /*? cnodesize ?*/;
